0b491329c3674fb3341274271b9809279cdf6b27
Baptiste <tabarbaptiste@gmail.com>
2024-05-22 15:49:58 +0200

    Licences

---
+++ view/licences.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Licences Professionnelles</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/licences.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container licences">
+        <h1>Licences Professionnelles</h1>
+        <div class="row">
+            <div class="col-md-9 gauche">
+                <div class="candidature mb-4">
+                    <h4>Déposer une candidature</h4>
+                    <p>Les candidatures à ces licences pour l'année 2024/2025 sont ouvertes jusqu'au 30 juin 2024.</p>
+                    <input type="button" value="Déposer une candidature en ligne">
+                </div>
+                <div class="image mb-4">
+                    <img src="./assets/img/banniere_informatique_2.jpg" alt="site_evry_le_pelvoux">
+                </div>
+                <div class="licence mb-4">
+                    <h2>Licence Pro Métiers de l’informatique : applications web</h2>
+                    <div class="presentation mb-3">
+                        <h4>Parcours ASR2I</h4>
+                        <p>Pour le Parcours Administration et Sécurisation des Réseaux et services Internet et Intranet
+                            (ASR2I), il
+                            est chargé de l’administration et la gestion d’un parc informatique, la supervision et la
+                            sécurisation
+                            d’un réseau local, et de l’intégration de produits et de services Intranet ou/et Internet.
+                            Les postes
+                            visés sont intermédiaires entre des techniciens supérieurs et des ingénieurs. Savoir-faire
+                            et compétences
+                            à l’issue de ce parcours :</p>
+                        <ul>
+                            <li>Modéliser des données pour mieux intégrer les services web, concevoir et réaliser des
+                                sites web
+                                Intranet et Internet, CMS, programmer côté client et serveur.</li>
+                            <li>Configurer un réseau local pour un ensemble de postes informatiques, dans un
+                                environnement
+                                hétérogène (Windows, Linux) et assurer l’interconnexion avec le monde extérieur.</li>
+                            <li>Administrer au quotidien un parc hétérogène de postes serveurs et bureautiques rattachés
+                                au réseau
+                                informatique pour garantir, en permanence, leur disponibilité et leur sécurité.</li>
+                            <li>Intégrer des solutions de sécurité des applications et des données.</li>
+                        </ul>
+                    </div>
+                    <div class="presentation mb-3">
+                        <h4>Parcours DAW2I</h4>
+                        <p>Pour le Parcours Développement avancé d’Applications Web Internet et Intranet (DAW2I), il est
+                            chargé de
+                            développement Web en Front-End et Back-End, et intégration de produits et de services
+                            Intranet ou/et
+                            Internet. Savoir-faire et compétences à l’issue de ce parcours :</p>
+                        <ul>
+                            <li>Modéliser des données pour mieux intégrer les services web, concevoir et réaliser des
+                                sites web
+                                Intranet et Internet, CMS, programmer côté client et serveur.</li>
+                            <li>
+                                <p>Développer des applications WEB à partir des langages de haut niveau après avoir
+                                    défini avec le
+                                    client les besoins et le cahier des charges.</p>
+                                <p>Intégrer des services et des applications WEB aussi bien pour un Intranet que pour
+                                    l’Internet.</p>
+                            </li>
+                            <li>Maîtriser et paramétrer les outils de collaboration entre les applications web.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="licence mb-4">
+                    <h2>Licence Pro Métiers de l'Informatique : Systèmes d'Information et Gestion de Données</h2>
+                    <div class="presentation mb-3">
+                        <h4>Parcours APSRA</h4>
+                        <ul>
+                            <li>Installer et administrer les composants systèmes (serveurs,...), réseaux, bases de
+                                données, serveurs
+                                et services d’application, mettre en production les applications et les progiciels
+                                fournis par les
+                                services études.</li>
+                            <li>Concevoir, intégrer et gérer les infrastructures techniques et applicatives (serveurs,
+                                réseaux,
+                                bases de données, serveurs d’application,…), de les industrialiser et les automatiser,
+                                afin de
+                                fournir des services de qualité adaptés aux enjeux métiers des entreprises.</li>
+                            <li>Etudier, installer et paramétrer les outils d’automatisation et de contrôle : automates
+                                d’exécution
+                                des traitements, des sauvegardes, outils de supervision, d’analyse de performance,...
+                            </li>
+                            <li>Analyser la conformité des résultats du système d’information en liaison avec les
+                                services
+                                utilisateurs.</li>
+                            <li>Mesurer la performance du système d’information (gestion des incidents, traçabilité,
+                                statistiques,
+                                mise en œuvre de plans de progrès ou de correction en liaison avec les études
+                                informatiques).</li>
+                        </ul>
+                    </div>
+                    <div class="presentation mb-3">
+                        <h4>Parcours ABDD</h4>
+                        <ul>
+                            <li>Concevoir des architectures de systèmes de bases de données et de systèmes décisionnels,
+                                ainsi que
+                                les interconnexions entre bases de données et systèmes décisionnels.</li>
+                            <li>Mener des analyses et du développement sur les bases de données et les systèmes
+                                décisionnels, en
+                                utilisant les langages de programmation appropriés.</li>
+                            <li>Conduire les projets sur l’ensemble de leur cycle, du recueil du besoin de l’utilisateur
+                                jusqu’à la
+                                transmission aux services en charge de l’exploitation.</li>
+                            <li>Expertiser des bases de données relationnelles existantes avant mise en place de
+                                systèmes
+                                décisionnels. La formation a un objectif de professionnalisation très fort. Chacun des
+                                deux
+                                parcours est destiné à former à des métiers parfaitement identifiés. En matière de
+                                débouchés, les
+                                diplômés peuvent intégrer directement le monde professionnel à l’issue de la formation.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 droite">
+                <div class="info mb-3">
+                    <i class="bi bi-geo-alt"></i>
+                    <h6>LOCALISATION</h6>
+                    <img src="./assets/img/site_evry_le_pelvoux.jpg" alt="site_evry_le_pelvoux">
+                    <p>36 rue du Pelvoux</p>
+                    <p>ZI de la Petite Montagne Nord</p>
+                    <p>91080 Courcouronnes</p>
+                </div>
+                <div class="info mb-3">
+                    <!-- <i class="bi bi-mortarboard"></i> -->
+                    <i class="fa-solid fa-graduation-cap"></i>
+                    <h6>DÉPARTEMENT</h6>
+                    <a href="#">Génie Électrique et Informatique Industrielle (GEII)</a>
+                </div>
+                <div class="info mb-3">
+                    <i class="bi bi-envelope-open"></i>
+                    <h6>RESPONSABLE(S)</h6>
+                    <p>Khalifa DJEMAL</p>
+                    <a href="mailto:k.djemal@iut.univ-evry.fr">k.djemal@iut.univ-evry.fr</a>
+                </div>
+                <div class="info mb-3">
+                    <i class="bi bi-envelope-open"></i>
+                    <h6>CONTACT(S) PÉDAGOGIQUE(S) ET ADMINISTRATIF(S)</h6>
+                    <p>Marie OCARIZ</p>
+                    <a href="mailto:m.ocariz@iut.univ-evry.fr">m.ocariz@iut.univ-evry.fr</a>
+                    <a href="tel:+33 1 69 47 72 21">01 69 47 72 21</a>
+                </div>
+                <div class="info mb-3">
+                    <i class="bi bi-clock-history"></i>
+                    <h6>DURÉE DE FORMATION</h6>
+                    <p>1 an</p>
+                </div>
+                <div class="info mb-3">
+                    <i class="bi bi-buildings"></i>
+                    <h6>RÉGIME D’INSCRIPTION</h6>
+                    <p>Formation en alternance</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
