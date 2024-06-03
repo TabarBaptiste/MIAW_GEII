3a5d7c913835fc832294c386694a26f79d55ba41
Baptiste <tabarbaptiste@gmail.com>
2024-06-03 16:19:14 +0200

    Ajout d'un footer

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $test = true;
 
 if ($test) {
@@ -13,13 +13,12 @@ if ($test) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=isset($page_title)?$page_title:"Accueil | GEII";?></title>
+    <title><?= isset($page_title) ? $page_title : "Accueil | GEII"; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous">
-    </script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
     <script src="https://kit.fontawesome.com/45e38e596f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="view/assets/css/style.css">
 </head>
@@ -28,11 +27,12 @@ if ($test) {
         <div class="navbar">
             <div>
                 <a href="index.php">
-                    <img src="view\assets\img\Logo-GEII\logo-medium.png" alt="logo Département Génie Electrique et Informatique Industrielle" id="logo">
+                    <img src="view\assets\img\Logo-GEII\logo-medium.png"
+                        alt="logo Département Génie Electrique et Informatique Industrielle" id="logo">
                 </a>
             </div>
             <a class="navbar_link" href="index.php?page=departement">Département</a>
-            <?php 
+            <?php
             if (isset($user)) {
                 switch ($user['role']) {
                     case 'etudiant':
@@ -56,52 +56,54 @@ if ($test) {
                 }
             }
             ?>
-            <p class="navbar_link"><?=isset($user)?$user['name']:"NOM Prénom";?> <i class="fas fa-chevron-down"></i></p>
+            <p class="navbar_link"><?= isset($user) ? $user['name'] : "NOM Prénom"; ?> <i
+                    class="fas fa-chevron-down"></i></p>
         </div>
     </div>
     <div class="content">
-        <?php 
+        <?php
         // Sélection de la page à charger selon l'URL
         if (!isset($_GET['page'])) {
-            require_once("view/accueil.html");
+            require_once ("view/accueil.html");
         } else {
             switch ($_GET['page']) {
                 case 'connexion':
-                    require_once("view/connexion.php");
+                    require_once ("view/connexion.php");
                     break;
                 case 'departement':
-                    require_once("view/departement.php");
+                    require_once ("view/departement.php");
                     break;
                 case 'espace_enseignant':
-                    require_once("view/espace_enseignant.php");
+                    require_once ("view/espace_enseignant.php");
                     break;
                 case 'espace_entreprise':
-                    require_once("view/espace_entreprise.php");
+                    require_once ("view/espace_entreprise.php");
                     break;
                 case 'espace_etudiant':
-                    require_once("view/espace_etudiant.php");
+                    require_once ("view/espace_etudiant.php");
                     break;
                 case 'form_alternance':
-                    require_once("view/form_alternance.php");
+                    require_once ("view/form_alternance.php");
                     break;
                 case 'form_tuteures':
-                    require_once("view/form_tuteures.php");
+                    require_once ("view/form_tuteures.php");
                     break;
                 case 'inscription':
-                    require_once("view/inscription.php");
+                    require_once ("view/inscription.php");
                     break;
                 case 'licences':
-                    require_once("view/licences.php");
+                    require_once ("view/licences.php");
                     break;
                 default:
-                    require_once("view/accueil.html");
+                    require_once ("view/accueil.html");
                     break;
             }
         }
         ?>
     </div>
-    <footer>
-        <h3>Footer</h3>
-    </footer>
-    </body>
+</div>
+<footer>
+    <?php require_once ("view/footer.php"); ?>
+</footer>
+
 </html>
---
+++ view/footer.php
@@ -0,0 +1,58 @@
+<div class="footer">
+    <div class="footer-haut container-fluid">
+        <div class="row">
+            <div class="col-md-4 gauche">
+                <h5>NOS DIFFÉRENTS SITES</h5>
+                <div class="pre">
+                    <h6>Site d’Évry</h6>
+                    <p>25 cours Monseigneur Roméro</p>
+                    <p>91000 Évry</p>
+                </div>
+                <div class="pre">
+                    <h6>Site de Brétigny</h6>
+                    <p>Chemin de la Tuilerie</p>
+                    <p>91220 Brétigny-sur-Orge</p>
+                </div>
+                <div class="pre">
+                    <h6>Site de Juvisy</h6>
+                    <p>6 A rue Piver – Parc de la Mairie</p>
+                    <p>91260 Juvisy-sur-Orge</p>
+                </div>
+            </div>
+            <div class="col-md-4 centre">
+                <h5>LIENS UTILES</h5>
+                <div class="pre-liens">
+                    <a href="https://www.univ-evry.fr/accueil.html" class="d-block text-white">Université d’Evry Val
+                        d’Essonne</a>
+                    <a href="https://www.iut-evry.fr/iut/presentation-de-liut/" class="d-block text-white">Présentation
+                        de l’IUT</a>
+                    <a href="https://www.iut-evry.fr/nos-formations/but/" class="d-block text-white">Liste des
+                        B.U.T</a>
+                    <a href="https://www.iut-evry.fr/nos-formations/lp/" class="d-block text-white">Liste des
+                        Licences
+                        Professionnelles</a>
+                    <a href="https://www.iut-evry.fr/scolarite/scolarite-infos-pratiques/"
+                        class="d-block text-white">Scolarité – Infos pratiques</a>
+                    <a href="https://www.iut-evry.fr/vie-etudiante/vie-pratique/" class="d-block text-white">Vie
+                        étudiante</a>
+                </div>
+            </div>
+            <div class="col-md-4 logo">
+                <div class="logo-gauche">
+                    <img src="C:\laragon\www\MIAW_GEII\view\assets\img\logo_footer.png" alt="Logo">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bas row mt-3">
+        <div class="col-md-6 footer_bas_gauche">
+            <p>Copyright © 2024 IUT d'Évry Val d'Essonne – Propulsé par Customify.</p>
+        </div>
+        <div class="col-md-6 footer_bas_droite text-right">
+            <p>Contact – Mentions légales</p>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
