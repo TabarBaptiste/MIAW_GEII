6ce144f67150a61f6d5a43998bbe05a70bc7de38
Baptiste <tabarbaptiste@gmail.com>
2024-06-03 14:25:39 +0200

    Récupération données main

--- index.php
+++ index.php
@@ -11,61 +11,60 @@ if ($test) {
 <html lang="fr">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= isset($page_title) ? $page_title : "Accueil | GEII"; ?></title>
+    <title><?=isset($page_title)?$page_title:"Accueil | GEII";?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous">
+    </script>
     <script src="https://kit.fontawesome.com/45e38e596f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="view/assets/css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="./assets/img/Logo-GEII/logo-medium.png">
 </head>
-<body>
-    <div class="container-fluid">
-        <div class="header">
-            <div class="navbar">
-                <div>
-                    <a href="index.php">
-                        <img src="view\assets\img\Logo-GEII\logo-medium.png" alt="logo Département Génie Electrique et Informatique Industrielle" id="logo">
-                    </a>
-                </div>
-                <a class="navbar_link" href="index.php?page=departement">Département</a>
-                <?php 
-                if (isset($user)) {
-                    switch ($user['role']) {
-                        case 'etudiant':
-                            // require_once("view/navbar/navbar_etudiant.php");
-                            echo "
-                            <a class=\"navbar_link\" href=\"index.php?page=edt\">Emploi du temps</a>
-                            <a class=\"navbar_link\" href=\"index.php?page=notes\">Notes</a>
-                            ";
-                            break;
-                        case 'enseignant':
-                            // require_once("view/navbar/navbar_enseignant.php");
-                            echo "
-                            <a class=\"navbar_link\" href=\"index.php?page=notes\">Notes</a>
-                            ";
-                            break;
-                        case 'entreprise':
-                            // require_once("view/navbar/navbar_entreprise.php");
-                            echo "
-                            <a class=\"navbar_link\" href=\"index.php?page=projet_tutore\">Projets tutorés</a>
-                            ";
-                            break;
-                    }
-                }
-                ?>
-                <p class="navbar_link"><?=isset($user)?$user['name']:"NOM Prénom";?> <i class="fas fa-chevron-down"></i></p>
+<div class="container-fluid">
+    <div class="header">
+        <div class="navbar">
+            <div>
+                <a href="index.php">
+                    <img src="view\assets\img\Logo-GEII\logo-medium.png" alt="logo Département Génie Electrique et Informatique Industrielle" id="logo">
+                </a>
             </div>
-        </div>
-        <div class="content">
+            <a class="navbar_link" href="index.php?page=departement">Département</a>
             <?php 
-            // Séléction de la page à charger selon l'URL
+            if (isset($user)) {
+                switch ($user['role']) {
+                    case 'etudiant':
+                        // require_once("view/navbar/navbar_etudiant.php");
+                        echo "
+                        <a class=\"navbar_link\" href=\"index.php?page=espace_etudiant\">Mon espace étudiant</a>
+                        ";
+                        break;
+                    case 'enseignant':
+                        // require_once("view/navbar/navbar_enseignant.php");
+                        echo "
+                        <a class=\"navbar_link\" href=\"index.php?page=espace_enseignant\">Mon espace enseignant</a>
+                        ";
+                        break;
+                    case 'entreprise':
+                        // require_once("view/navbar/navbar_entreprise.php");
+                        echo "
+                        <a class=\"navbar_link\" href=\"index.php?page=espace_entreprise\">Mon espace entreprise</a>
+                        ";
+                        break;
+                }
+            }
+            ?>
+            <p class="navbar_link"><?=isset($user)?$user['name']:"NOM Prénom";?> <i class="fas fa-chevron-down"></i></p>
+        </div>
+    </div>
+    <div class="content">
+        <?php 
+        // Sélection de la page à charger selon l'URL
+        if (!isset($_GET['page'])) {
+            require_once("view/accueil.html");
+        } else {
             switch ($_GET['page']) {
                 case 'connexion':
                     require_once("view/connexion.php");
@@ -98,11 +97,11 @@ if ($test) {
                     require_once("view/accueil.html");
                     break;
             }
-            ?>
-        </div>
+        }
+        ?>
     </div>
     <footer>
         <h3>Footer</h3>
     </footer>
-</body>
+    </body>
 </html>
